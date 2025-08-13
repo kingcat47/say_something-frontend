34ebda5154d7b95f9ca5440b4ce98f3e38079177
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-13 09:30:58 +0900

    아니 도플로이 왜 안되냐고고고고고고ㅓㄱ

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Main_Page from "./page/Main_Page";
+// import Main_Page from "./page/Main_Page";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        children: [
-            { path: "main", element: <Main_Page /> },
-
-        ],
+        // children: [
+        //     { path: "main", element: <Main_Page /> },
+        //
+        // ],
     },
 ]);
 
