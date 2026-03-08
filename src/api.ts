e2d08595b2f44902d7479be38396eddb1164e76a
orangejack <79419803+kingcat47@users.noreply.github.com>
2026-03-09 05:46:55 +0900

    ㅇ

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,3 @@
 export const getApiBaseUrl = () => {
-  return "say-something-frontend-d06znjusd-kingcat47s-projects.vercel.app";
+  return "https://say-something-backend-atdx.vercel.app";
 };
--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  "https://say-something-frontend-d06znjusd-kingcat47s-projects.vercel.app:"
-);
+export const socket = io("https://say-something-backend-atdx.vercel.app/");
