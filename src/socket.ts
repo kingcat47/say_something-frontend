d49e45552b67189b3bdae9b1ea366244c90b81ba
orangejack <79419803+kingcat47@users.noreply.github.com>
2026-06-15 19:50:21 +0900

    ㅑ

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://strewn-dander-fidgeting.ngrok-free.dev/";
+const SERVER_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://strewn-dander-fidgeting.ngrok-free.dev";
 export const socket = io(SERVER_URL);
