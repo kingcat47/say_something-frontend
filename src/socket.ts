99d01b7fb7f5044e1c5e9eb994e783900bae2e8e
orangejack <79419803+kingcat47@users.noreply.github.com>
2026-06-15 19:53:07 +0900

    ngrok 하튜하튜

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,8 @@
 import { io } from "socket.io-client";
 
 const SERVER_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://strewn-dander-fidgeting.ngrok-free.dev";
-export const socket = io(SERVER_URL);
+export const socket = io(SERVER_URL, {
+    extraHeaders: {
+        "ngrok-skip-browser-warning": "true",
+    },
+});
