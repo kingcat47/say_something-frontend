b50902c0f83f33ba58bdea7dbd5cb9f8caa6d99e
orangejack <79419803+kingcat47@users.noreply.github.com>
2026-06-17 11:43:50 +0900

    시연용 잠깐깐

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.DEV ? 'http://localhost:3000' : 'https://strewn-dander-fidgeting.ngrok-free.dev';
+const BASE_URL = 'https://strewn-dander-fidgeting.ngrok-free.dev';
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = import.meta.env.DEV ? "http://localhost:3000" : "https://strewn-dander-fidgeting.ngrok-free.dev";
+const SERVER_URL = "https://strewn-dander-fidgeting.ngrok-free.dev";
 export const socket = io(SERVER_URL, {
     extraHeaders: {
         "ngrok-skip-browser-warning": "true",
